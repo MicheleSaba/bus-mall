b29d27c77706e371142c183a477beb8e7d1262b3
Michele Saba <michelemsaba@gmail.com>
2019-03-26 14:34:19 -0500

    complete lab16

--- app.js
+++ app.js
@@ -1,65 +1,86 @@
 
-function RandomImage (name, imageAddress) {
-  this.name = name; 
-  this.clicks = 0; 
-  this.imageAddress = imageAddress;
-  this.totalViews = 0; 
+function RandomImage (name) {
+  this.name = name;
+  this.clicks = 0;
+  this.src = '/img/' + name + '.jpg';
+  this.totalViews = 0;
 }
 
 var totalClicks = 0; 
 
-
 var randomArray = [
-  Bathroom = new RandomImage(' bathroom object ','./img/bathroom.jpg'),
-  Banana = new RandomImage(' banana object ','./img/banana.jpg'),
-  Boots = new RandomImage(' boots object ','./img/boots.jpg'),
-  Breakfast = new RandomImage(' breakfast object ','./img/breakfast.jpg'),
-  Bag = new RandomImage(' bag object ','./img/bag.jpg'),
-  BubbleGum = new RandomImage(' bubbleGum object ','./img/bubblegum.jpg'),
-  Chair = new RandomImage('chair object ','./img/chair.jpg'),
-  Dog = new RandomImage('chair object ','./img/dog-duck.jpg')
+  new RandomImage('bathroom'),
+  new RandomImage('banana'),
+  new RandomImage('boots'),
+  new RandomImage('breakfast'),
+  new RandomImage('bag'),
+  new RandomImage('bubbleGum'),
+  new RandomImage('chair'),
 ];
 
 function makeRandom(){
   return Math.floor(Math.random() * randomArray.length);
 }
 
- function render() {
-  // for (var i = 0; i < 3; i++) {
+function render() {
   var randomNumber1 = makeRandom();
   var randomNumber2 = makeRandom();
-  console.log(randomArray[randomNumber2].imageAddress + randomNumber2 );
   while (randomNumber1 === randomNumber2) {
     randomNumber1 = makeRandom();
   }
-  var img1 = document.createElement('img');
-  img1.src = randomArray[randomNumber1].imageAddress;
-  document.body.appendChild(img1);
-  document.body.appendChild(document.createElement('IMG')).src = randomArray[randomNumber2].imageAddress;
+ 
+  var img1 = document.getElementById('img1');
+  img1.setAttribute('data-index', randomNumber1);
+  img1.src = randomArray[randomNumber1].src;
+  
+  randomArray[randomNumber1].totalViews++;
 
+
+  var img2 = document.getElementById('img2');
+  img2.setAttribute('data-index', randomNumber2);
+  img2.src = randomArray[randomNumber2].src;
+  
   var randomNumber3 = makeRandom();
-  console.log(randomArray[randomNumber1].imageAddress + randomNumber1); 
   while (randomNumber3 === randomNumber1 || randomNumber3 === randomNumber2) {
     randomNumber3 = makeRandom();
   }
-
-  document.body.appendChild(document.createElement('img')).src = randomArray[randomNumber3].imageAddress;
-  console.log(randomArray[randomNumber3].imageAddress);
+  var img3 = document.getElementById('img3');
+  img3.setAttribute('data-index', randomNumber3);
+  img3.src = randomArray[randomNumber3].src;
 }
 
-
-function handleClick() {
-  console.log("user clicked!");
-  totalClicks ++
-  console.log(totalClicks);
-}
-var imgTags = document.getElementsByTagName('IMG');
+var imgTags = document.getElementsByTagName('img');
 
 function imageClicks () {
   for (var i=0;i<imgTags.length;i++) {
-    imgTags[i].addEventListener("click", handleClick);
-    // console.table(imgTags[i]);
+    imgTags[i].addEventListener('click', handleClick);
   }
+}
+
+function handleClick(event) {
+  randomArray[event.target.getAttribute('data-index')].clicks++;
+  totalClicks ++;
+  if (totalClicks >= 10) {
+    createText();
+    for (var i=0;i<imgTags.length;i++) {
+      imgTags[i].removeEventListener('click', handleClick);
+    }
+  }
+  render();
+  console.log(totalClicks);
+}
+
+
+
+
+function createText() {
+  var unordered = document.createElement('ul');
+  for(var i = 0; i < randomArray.length; i++) {
+    var data = document.createElement('li');
+    data.textContent = randomArray[i].clicks + randomArray[i].name ;
+    unordered.appendChild(data);
+  }
+  document.body.appendChild(unordered);
 }
 
 render();
